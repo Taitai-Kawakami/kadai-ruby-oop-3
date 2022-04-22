1e2b4d0b077dd2986d896a3a054655cd1842faac
Taitai-Kawakami <tai.zkx@gmail.com>
2022-04-22 15:40:32 +0000

    first commit

---
+++ human.rb
@@ -0,0 +1,19 @@
+require './animal'
+require './thinkable'
+
+class Human < Animal
+  include Thinkable
+  
+  attr_accessor :hobby
+  
+  def initialize(name, age, hobby)
+    super(name, age)
+    self.hobby = hobby
+  end
+end
+
+
+#テスト
+# tanaka = Human.new("田中 太郎", "25", "電車")
+# tanaka.say
+# tanaka.think
